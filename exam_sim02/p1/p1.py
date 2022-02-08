69e520a50d266c95ea56e6bca14dc710f79a336b
Your Name <you@example.com>
2022-02-08 09:53:31 -0400

    exam 1

--- exam_sim02/p1/p1.py
+++ exam_sim02/p1/p1.py
@@ -5,3 +5,19 @@ Created on Tue Feb  8 07:18:36 2022
 @author: ACER
 """
 
+lista =("Introduce una frase")
+diccionario = []
+for palabra in lista:
+    if palabra in diccionario:
+       diccionaro[palabra]= diccionario [palabra]+ 1
+    else:
+        diccionario[palabra]= 0
+        
+print("%s se repite %s veces" % (palabra, diccionario))
+
+       
+        
+        
+        
+        
+        
--- exam_sim02/p3/p3.py
+++ exam_sim02/p3/p3.py
@@ -5,3 +5,12 @@ Created on Tue Feb  8 07:22:08 2022
 @author: ACER
 """
 
+lista= "hola mundo, soy adri mundo!"
+listaSplit = lista.split()
+for word in listaSplit:
+     if word in tempLista:
+        tempList[word] += 1
+    
+     else:
+        tempLista[word]= 1
+print(tempLista)  
