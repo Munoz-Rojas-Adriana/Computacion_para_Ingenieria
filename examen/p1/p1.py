af20eed3758178ade687cd08abf297167441a48d
Your Name <you@example.com>
2022-02-09 08:46:20 -0400

    examen p1 p2

--- examen/p1/p1.py
+++ examen/p1/p1.py
@@ -5,3 +5,34 @@ Created on Wed Feb  9 07:18:06 2022
 @author: ACER
 """
 
+def deNumero_aListaInvertida(nombre):
+    lista = []
+    while nombre > 0:
+        lista.append(int(nombre%10))
+        nombre = int(nombre/10)
+    return lista 
+
+def invertir_lista (lista):
+    nueva_lista = []
+    tamaño_lista = len(lista)
+    while tamaño_lista > 0:
+        palabra-sacado = lista[tamaño_lista-1]
+        nueva_lista.append(palabra-sacado)
+        tamaño_lista =tamaño_lista-1
+    return nueva_lista
+
+def esPalindromo(palabra):
+    lista = dePalabra_aListaInvertida(palabra)
+    lista_invertida = invertir_lista(lista)
+    if lista == lista_invertida:
+        print("SI ES PALINDROMA")
+    else:
+        print("NO ES PALINDROMA")
+        
+        
+
+    
+    
+    
+        
+    
--- examen/p2/p2.py
+++ examen/p2/p2.py
@@ -5,3 +5,21 @@ Created on Wed Feb  9 07:18:47 2022
 @author: ACER
 """
 
+#pregunta 2
+
+lista = [ 10,20,30,10,5,1,3,5,4]
+
+lista_pares= []
+lista_impares=[]
+
+for i in lista:
+    if i%2 == 0:
+        lista_pares.append(i)
+    else:
+        lista_impares.append(i)
+        
+print("Lista pares: ")
+print(lista_pares)
+print("Lista Impares")
+print(lista_impares)
+
