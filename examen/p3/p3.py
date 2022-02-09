d61ec2a270f5ec08f0161edd03fd7eaa302a4e14
Your Name <you@example.com>
2022-02-09 09:34:53 -0400

    examen p3

--- examen/p3/p3.py
+++ examen/p3/p3.py
@@ -5,3 +5,31 @@ Created on Wed Feb  9 07:19:17 2022
 @author: ACER
 """
 
+archnotas = open('notas.txt', 'r')
+archestudiantes = open('estudiantes.txt', 'r')
+
+lista_notas = archnotas.readlines()
+lista_estudiantes = archestudiantes.readlines()
+
+for i in range(len(lista_estudiantes)):
+    lista_notas[i] = lista_notas[i].strip()
+    lista_estudiantes = lista_estudiantes[i].strip()
+    
+     
+archPrimer_Parcial= open('Primer_Parcial.txt', 'wt')  
+
+archPrimer_Parcial.write(lista_notas[0]+" "+lista_estudiantes[0])
+archPrimer_Parcial.write(lista_estudiantes[2]+" "+lista_notas[2])
+
+for i in range(len(lista_notas)):
+    archPrimer_Parcial.write(lista_notas[i]+" "+lista_estudiantes+"\n")
+    
+
+    
+archPrimer_Parcial.close()
+archnotas.close()
+archestudiantes.close()
+
+
+
+    
