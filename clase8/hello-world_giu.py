fa9a2b8a8734b00e772b4e018e04472330cf90ad
Your Name <you@example.com>
2022-02-16 00:13:31 -0400

    clase8

---
+++ clase8/hello-world_giu.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 00:06:49 2022
+
+@author: ACER
+"""
+
+# clase 8 / hello_world_gui.py
+
+from tkinter import *
+
+root = Tk ()
+a = Label(root, text="hello World")
+a.pack()
+root.mainloop()
+
+
