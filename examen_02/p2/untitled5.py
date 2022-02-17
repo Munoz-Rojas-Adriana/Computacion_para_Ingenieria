4f695887966a94b6c422d100c2c4ec4a94d1e80e
Your Name <you@example.com>
2022-02-17 10:07:41 -0400

    p5

--- examen_02/p2/untitled5.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 17 09:49:21 2022
-
-@author: ACER
-"""
-
--- examen_02/p5/p5.py
+++ examen_02/p5/p5.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 17 09:14:08 2022
+Created on Thu Feb 17 10:01:43 2022
 
 @author: ACER
 """
 
 class Animal:
     
-    def __init__(self, comer, habla, corre,):
-        self.comer=comer
-        self.habla=habla
-        self.corre=corre
+    def __init__(self, comer, caminar ):      
+        self.comer = comer
+        self.caminar = caminar
+       
         
-        
-        
+    @property
+    def getcomer(self):
+        return self.comer
+    
+    def getcaminar(self):
+        return self.caminar
+    
+    def setcomer(self, new_comer):
+        self.comer = new_comer
+    
+    def setfcaminar(self, new_caminar):
+        self.caminar = new_caminar
         
     
